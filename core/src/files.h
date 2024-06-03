edcda65acd2b5c6d9bf22016f43cc42fd57a5795
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-06-03 17:26:32 +0300

    chore: remove not used struct

--- core/src/files.h
+++ core/src/files.h
@@ -8,11 +8,6 @@
 #include "constants.h"
 #include "leafs.h"
 
-struct Header {
-  char name[3];
-  short version;
-};
-
 std::ofstream preparePktFile(fs::path filename);
 bool validateHeader(std::ifstream &os);
 lluint padFileSize(lluint size);
